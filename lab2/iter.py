3ecbfe06362a2b85c9b5311864b565ffada62b1d
satturn <satturn@bitbucket.org>
2019-02-27 19:55:56 +0300

    lab2

--- lab2/iter.py
+++ lab2/iter.py
@@ -2,39 +2,81 @@ import numpy as np
 import math
 np.set_printoptions(precision=3)
 
-def read_matrix(fname: str) -> np.array:                    #чтение матрицы из файла
+#чтение матрицы из файла
+def read_matrix(fname: str) -> np.array:  
     matrix = []
-    with open("./matrix.txt", 'r') as file_mat:
+    with open(fname, 'r') as file_mat:
         for line in file_mat.readlines():
             matrix.append(line.strip().split(" "))
-    matrix = np.array(matrix, dtype = float)
+    matrix = np.array(matrix, dtype=float)
     return matrix
 
-def norm(arr: np.array):                                    #нахождение нормы матриц
+# нахождение нормы матрицы
+def norm(arr: np.array):  
     return max([np.sum(abs(line)) for line in arr])
 
-def apply_mat(A: np.array, x: np.array):
+# умножение матрицы и вектора
+def apply_mat(A: np.array, x: np.array):  
     return np.array([np.sum(x*A[i]) for i in range(len(A))])
 
-def iter(B: np.array, b: np.array):
-    B = B.copy()
+# решение системы методом Гаусса
+def gaussian_elim(A: np.array, b: np.array) -> np.array:  
+    A = A.copy()
     b = b.copy()
-    x0 = np.array([1/2 for i in range(len(b))])
-    x = apply_mat(B, x0) + b
-    k = math.floor(math.log(0.01/(norm(x-x0))*(1-norm(B)), norm(B)))
-    for i in range(100500):
-        x = apply_mat(B, x) + b
+    for i in range(0, len(A)):  # прямой ход
+        b[i] /= A[i, i]
+        A[i] /= A[i, i]
+        for j in range(i+1, len(A)):
+            b[j] -= b[i]*A[j, i]
+            A[j] -= A[i]*A[j, i]
+    x = b
+    for k in range(len(A)-1, -1, -1):  # обратный ход
+        for m in range(len(A)-1, k, -1):
+            x[k] -= (A[k, m]*x[m])/A[k, k]
     return x
 
-mat = read_matrix("/home/mora/matrix.txt")
+# решение системы методом простых итераций(метод Якоби)
+def jacobi_method(A: np.array, b: np.array) -> np.array:
+    A = A.copy()
+    b = b.copy()
+    E = np.identity(len(A))
+    for i in range(0, len(A)):
+        b[i] /= A[i, i]
+        A[i] /= A[i, i]
+    B = E - A
+    if norm(B) < 1:
+        x0 = np.array([1/2 for i in range(len(b))])
+        x = apply_mat(B, x0) + b
+        k = math.floor(math.log(0.01/(norm(x-x0))*(1-norm(B)), norm(B)))
+        for i in range(0, k+1):
+            x = apply_mat(B, x) + b
+        return x
+
+# решение системы методом Гаусса-Зейделя
+def gauss_seidel_method(A: np.array, b: np.array) -> np.array:
+    A = A.copy()
+    b = b.copy()
+    E = np.identity(len(A))
+    for i in range(0, len(A)):
+        b[i] /= A[i, i]
+        A[i] /= A[i, i]
+    B = E - A
+    if np.sum([abs(B[i,i]) > np.sum(B[i]) - B[i,i] for i in range(len(A))]):
+        x0 = np.array([1/2 for i in range(len(b))])
+        x = np.array(apply_mat(B, x0) + b)
+        k = math.floor(math.log(0.01/(norm(x-x0))*(1-norm(B)), norm(B)))
+        for n in range(k-1):
+            for i in range(len(A)):
+                x[i] = np.sum(B[i, :i]*x[:i]) + np.sum(B[i, i+1:len(A)]*x0[i+1:len(A)]) + b[i]
+            x0 = x
+        return x
+
+mat = read_matrix("./matrix.txt")
 A = mat[:, :-1]
 b = mat[:, -1]
-E = np.identity(len(A))
 
-for i in range(0, len(A)):
-    A[i]/=-A[i,i]
-    b[i]/=A[i,i]
-    A[i,i]=0
-B = A
-if (norm(B) < 1):
-    print(iter(B, b))
+
+print(f"Matrix A:\n{A}\nb column:\n {b}")
+print(f"Gaussian elimination: {gaussian_elim(A, b)}")
+print(f"Jacobi iterative method: {jacobi_method(A, b)}")
+print(f"Gauss-Seidel iterative method: {gauss_seidel_method(A, b)}")
